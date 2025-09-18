0987793fc868862739565ec08eacaee967090a43
kbsm <kbsm2807@gmail.com>
2025-09-18 04:44:29 +0100

    simpletest.sh

--- simpletest.sh
+++ simpletest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+person="A710950"
 echo "Hello from simpletest.sh"
 echo "This is a simple test script."
-echo "It demonstrates basic shell scripting. developed by A710950"
+echo "It demonstrates basic shell scripting. developed by $person"
 echo "Goodbye!"
 exit 0
 
